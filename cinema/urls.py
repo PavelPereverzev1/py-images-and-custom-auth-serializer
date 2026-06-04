9d2a3a7cdb706978d1a4f7160526140faa9fce30
PavelPereverzev1 <pereverzev.pavel1975@gmail.com>
2026-06-04 23:38:39 +0300

    feat: solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -18,6 +18,7 @@ router.register("movies", MovieViewSet)
 router.register("movie_sessions", MovieSessionViewSet)
 router.register("orders", OrderViewSet)
 
+
 urlpatterns = [path("", include(router.urls))]
 
 app_name = "cinema"
